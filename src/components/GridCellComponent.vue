bea3325e6e913dcc3d3a7aaf576561f48878ac89
gustavo-tomas <guga.tomas10@gmail.com>
2022-10-23 09:21:33 -0300

    Fixed faulty urls

--- src/components/GridCellComponent.vue
+++ src/components/GridCellComponent.vue
@@ -1,11 +1,15 @@
 <template>
-  <a :href="url" target="_blank">
-    <img
-      :src="require(`@/assets/images/${name}`)"
-      :title="title"
-      :alt="`${name}`"
-    />
-  </a>
+  <div class="image-container" ref="imageContainer" @click="toggleView">
+    <a :href="url" target="_blank" :alt="name">
+      <img
+        :src="require(`@/assets/images/${name}`)"
+        :title="title"
+        :alt="`${name}`"
+        class="image"
+        ref="image"
+      />
+    </a>
+  </div>
 </template>
 
 <script>
@@ -15,6 +19,20 @@ export default {
     name: String,
     url: String,
     title: String,
+  },
+  methods: {
+    toggleView() {
+      const imageContainer = this.$refs.imageContainer;
+      const image = this.$refs.image;
+
+      if (imageContainer.classList.contains("full-image")) {
+        imageContainer.classList.remove("full-image");
+        image.classList.add("image");
+      } else {
+        imageContainer.classList.add("full-image");
+        image.classList.remove("image");
+      }
+    },
   },
 };
 </script>
@@ -26,11 +44,23 @@ img {
   transition: all 0.3s ease-in-out;
 }
 
-a {
-  display: flex;
+.image:hover {
+  transform: scale(1.2);
+  transition: all 0.5s ease;
 }
 
-img:hover {
-  transform: scale(1.2);
+.full-image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 99;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  transition: all 0.5s ease;
 }
 </style>
--- src/views/RendersView.vue
+++ src/views/RendersView.vue
@@ -13,22 +13,10 @@ export default {
   data() {
     return {
       cells: [
-        {
-          name: "headphone.png",
-          url: "src/assets/images/headphone.png",
-        },
-        {
-          name: "flower_closeup.png",
-          url: "src/assets/images/flower_closeup.png",
-        },
-        {
-          name: "cell_shaded.png",
-          url: "src/assets/images/cell_shaded.png",
-        },
-        {
-          name: "spicy_314_front.png",
-          url: "src/assets/images/spicy_314_front.png",
-        },
+        { name: "headphone.png" },
+        { name: "flower_closeup.png" },
+        { name: "cell_shaded.png" },
+        { name: "spicy_314_front.png" },
       ],
     };
   },
